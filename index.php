86521687788aaf27b1a75bc39a00f9481daa90bb
Aleksandar Naumovic <corleoneqq@gmail.com>
2021-03-03 18:25:06 +0100

    o meni

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="still.css" type="text/css">
+    
+</head>
+<body>
+    <div id="okvir">
+        <div id="header">
+            <div id="header-logo">
+                <img src="images/logo.png" alt="itachi">
+            </div>
+            <div id="header-login">LOGOVANJE</div>
+        </div>
+
+        <div id="navigacija">
+            <ul>
+                <li class="link"><a href="#">Link 1</a></li>
+                <li class="link"><a href="#">Link 2</a></li>
+                <li class="link"><a href="#">Link 3</a></li>
+                <li class="link"><a href="#">Link 4</a></li>
+                <li class="link"><a href="#">Link 5</a></li>
+            </ul>
+        </div>
+
+        <div id="sadrzaj">
+            <div id="about-me-wrapper">
+                <div id="about-me-logo">
+                    <img src="images/ja.jpg" alt="itachi">
+                </div>
+                <div id="bio">
+                    OVDE IDE TEKST O MENI
+                </div>
+            </div>
+        </div>
+        <div id="cistac"></div>
+
+        <div id="footer">
+            FOOTER
+        </div>
+    </div>
+</body>
+</html>
