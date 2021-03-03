0198315fe80279213f917783d4d431bf5777c763
tamilov3 <tamaranaumovic@gmail.com>
2021-03-03 18:35:08 +0100

    izvuceno zaglavlje i footer

--- index.php
+++ index.php
@@ -1,31 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="still.css" type="text/css">
-    
+
 </head>
+
 <body>
     <div id="okvir">
-        <div id="header">
-            <div id="header-logo">
-                <img src="images/logo.png" alt="itachi">
-            </div>
-            <div id="header-login">LOGOVANJE</div>
-        </div>
-
-        <div id="navigacija">
-            <ul>
-                <li class="link"><a href="#">Link 1</a></li>
-                <li class="link"><a href="#">Link 2</a></li>
-                <li class="link"><a href="#">Link 3</a></li>
-                <li class="link"><a href="#">Link 4</a></li>
-                <li class="link"><a href="#">Link 5</a></li>
-            </ul>
-        </div>
+        <?php include("zaglavlje.php");?>
 
         <div id="sadrzaj">
             <div id="about-me-wrapper">
@@ -39,9 +26,8 @@
         </div>
         <div id="cistac"></div>
 
-        <div id="footer">
-            FOOTER
-        </div>
+    <?php include("footer.php")?>
     </div>
 </body>
+
 </html>
---
+++ zaglavlje.php
@@ -0,0 +1,18 @@
+<div id="zaglavlje">
+    <div id="header">
+        <div id="header-logo">
+            <img src="images/logo.png" alt="itachi">
+        </div>
+        <div id="header-login">LOGOVANJE</div>
+    </div>
+
+    <div id="navigacija">
+        <ul>
+            <li class="link"><a href="#">Link 1</a></li>
+            <li class="link"><a href="#">Link 2</a></li>
+            <li class="link"><a href="#">Link 3</a></li>
+            <li class="link"><a href="#">Link 4</a></li>
+            <li class="link"><a href="#">Link 5</a></li>
+        </ul>
+    </div>
+</div>
