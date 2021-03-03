48d627cc92335f37de4fbb38dc13abd3f0461ccb
tamilov3 <tamaranaumovic@gmail.com>
2021-03-03 19:09:44 +0100

    izmesten css za login stranu

--- login.php
+++ login.php
@@ -7,7 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login/Registracija</title>
 
+    <link rel="stylesheet" href="login.css" type="text/css">
     <link rel="stylesheet" href="still.css" type="text/css">
+
 
 </head>
 
