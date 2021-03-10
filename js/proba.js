efab0954072ebe0dcfac1f26f6e6bed21655d96a
Aleksandar Naumovic <corleoneqq@gmail.com>
2021-03-10 23:17:07 +0100

    i ovo

--- js/proba.js
+++ js/proba.js
@@ -2,4 +2,3 @@ const nazivProizvoda = document.getElementById("nazivProd");
 const cenaProizvoda = document.getElementById("cenaProd");
 const proizvodjac = document.getElementById("proizvodjacProd");
 const kolicina = document.getElementById("kvantitetProd");
-const 
